5a68a1943657c6882a2838f4e4fcce48f6e6da1b
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-05-29 14:16:06 +0200

    store

---
+++ src/store/store.js
@@ -0,0 +1,10 @@
+export const initialState = () => ({ contactList: [] });
+
+export default function storeReducer(state, action) {
+  switch (action.type) {
+    case "UPDATE_LIST":
+      return { ...state, contactList: [action.payload] };
+    default:
+      return state;
+  }
+}
