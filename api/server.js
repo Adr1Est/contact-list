98a50ffab4de9b5b9e4db09595b8cad11a49b7b3
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-05-29 10:24:01 +0200

    crear api

---
+++ api/server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const mockData = {
+  key: '2352342zsx423',
+  name: 'Adrián',
+  lastName: 'Estévez Salamanca',
+  email: 'adrian22est@gmail.com',
+  phone: '660487026',
+  city: 'Madrid',
+  country: 'España'
+};
+
+let contactList = [mockData];
+
+app.get('/contacts', (req, res) => {
+  res.json(contactList);
+});
+
+app.post('/contacts', (req, res) => {
+  const newContact = req.body;
+  contactList.push(newContact);
+  res.status(201).json(newContact);
+});
+
+app.put('/contacts/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  if (id >= 0 && id < contactList.length) {
+    contactList[id] = req.body;
+    res.json(contactList[id]);
+  } else {
+    res.status(404).send('Not found!');
+  }
+});
+
+app.delete('/contacts/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  if (id >= 0 && id < contactList.length) {
+    const deletedContact = contactList.splice(id, 1);
+    res.json(deletedContact);
+  } else {
+    res.status(404).send('Not found!');
+  }
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto: ${PORT}`);
+});
